7cd7df8ab7573a592c24ab8ca5f7ea9583315ac5
r-currie <currierobertm@gmail.com>
2019-12-24 14:16:19 -0600

    Default shows top tracks, but does not show art

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import axios from "axios";
 import "./App.css";
 import List from "./components/List";
 import Searchbar from "./components/Searchbar";
+import NewReleases from "./components/NewReleases";
 
 class App extends React.Component {
   state = {
-    albums: []
+    albums: [],
+    newReleases: []
   };
 
   // todo: Add options to search for artist, song title, album, etc and pass that through state
+
+  getNewReleases = async () => {
+    const res = await axios.get(
+      "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=b51e79f64b0918bfabb85f9627eabbd5&format=json"
+    );
+
+    console.log(res.data.tracks.track);
+    this.setState({ newReleases: res.data.tracks.track });
+  };
 
   onSearchSubmit = async term => {
     const res = await axios.get(
       `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${term}&api_key=b51e79f64b0918bfabb85f9627eabbd5&format=json`
     );
 
-    console.log(term);
     this.setState({ albums: res.data.topalbums.album, loading: false });
   };
 
@@ -25,6 +35,10 @@ class App extends React.Component {
       <div className="ui container" style={{ paddingTop: "20px" }}>
         <Searchbar onSubmit={this.onSearchSubmit} />
         <List albums={this.state.albums} />
+        <NewReleases
+          getNewReleases={this.getNewReleases}
+          songs={this.state.newReleases}
+        />
       </div>
     );
   }
